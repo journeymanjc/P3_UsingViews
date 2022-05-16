24ef60b927e518633649776678a7ad67818d107a
Jae Cho <jjaecho@gmail.com>
2022-05-16 14:21:12 -0400

    Initial Commit

---
+++ 100DaysOfSwiftExperiments/_00DaysOfSwiftExperimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00DaysOfSwiftExperimentsApp.swift
+//  100DaysOfSwiftExperiments
+//
+//  Created by Jae Cho on 5/16/22.
+//
+
+import SwiftUI
+
+@main
+struct _00DaysOfSwiftExperimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
